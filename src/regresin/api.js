f33684ba03f9a7eb66c789699b86413fbdd962b3
margarita.vladyko <margarita.vladyko@itglobal.com>
2024-06-23 21:51:32 +0300

    add example use URLSearchParams and filters for fetchUser

--- src/regresin/api.js
+++ src/regresin/api.js
@@ -1,8 +1,10 @@
 const baseUrl = 'https://reqres.in/api'
 
 // eslint-disable-next-line no-unused-vars
-async function fetchUsers() {
-  const response = await fetch(`${baseUrl}/users`)
+async function fetchUsers(queryParams = {}) {
+    const response = await fetch(
+      `${baseUrl}/users?${new URLSearchParams(queryParams)}`,
+    )
   return {
     status: response.status,
     headers: response.headers,
@@ -80,9 +82,9 @@ async function fetchUser(id) {
 
 ;(async () => {
   try {
-    const response = await createUser({
-      name: 'Margarita Vladyko',
-      job: 'AQA Engineer',
+    const response = await fetchUsers({
+      page: 100,
+      per_page: 1,
     })
     console.log('status', response.status)
     console.log('data', response.data)
