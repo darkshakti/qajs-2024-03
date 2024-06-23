75e8ebedf0fa58dbb72e714fbbdeb2da22e9318e
margarita.vladyko <margarita.vladyko@itglobal.com>
2024-06-23 23:42:50 +0300

    first object

---
+++ src/obj/firstObject.js
@@ -0,0 +1,12 @@
+const student = {
+  id: 1,
+  name: 'Margarita Vladyko',
+  isActive: true,
+  roles: ['student', 'aqa_engineer'],
+  social: {
+    x: 'https://twitter.com/user',
+    vk: 'https://vk.com/user',
+  },
+}
+
+console.log(student)
