2465addd9758634778ea33ca83dbbb253ef7d2e9
margarita.vladyko <margarita.vladyko@itglobal.com>
2024-07-14 17:12:21 +0300

    got example

--- jest.config.mjs
+++ jest.config.mjs
@@ -181,6 +181,7 @@ const config = {
   //   "\\\\node_modules\\\\",
   //   "\\.pnp\\.[^\\\\]+$"
   // ],
+  transformIgnorePatterns: ['/node_modules/(?!got)/'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
