51c5b44226e9dff678b015cdb271dfc5c10b6008
margarita.vladyko <margarita.vladyko@itglobal.com>
2024-08-04 19:22:16 +0300

    example addMsg jest-html-reporter

--- tests/bookstore/auth.test.js
+++ tests/bookstore/auth.test.js
@@ -1,13 +1,15 @@
-/* eslint-disable jest/no-disabled-tests */
 import config from '../../framework/config/configBookstore'
 import { AuthService } from '../../framework'
+import { addMsg } from 'jest-html-reporters/helper'
 
 describe('Авторизация', () => {
   it('Успешная авторизация', async () => {
-    const response = await AuthService.generateTokenCached({
+    const credentials = {
       userName: config.userName,
       password: config.password,
-    })
+    }
+    await addMsg({message: `Доступы: ${JSON.stringify(credentials, null, 2)}`})
+    const response = await AuthService.generateTokenCached(credentials)
 
     expect(response.status).toBe(200)
     expect(response.data.result).toBe('User authorized successfully.')
