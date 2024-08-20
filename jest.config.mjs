2c565d3b97dee630527d517b0f319e88cece2950
margarita.vladyko <margarita.vladyko@itglobal.com>
2024-08-20 03:34:53 +0300

    add playwright

--- jest.config.mjs
+++ jest.config.mjs
@@ -158,6 +158,7 @@ const config = {
   //   "**/__tests__/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
+  testMatch: ['**/tests/**/?(*.)+(spec|test).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
